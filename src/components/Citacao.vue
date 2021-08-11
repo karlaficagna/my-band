f0fdd5ebf6c64885e44e36074079eb0ad73d9514
Karla Ficagna <karlaficagna@gmail.com>
2021-08-10 22:29:08 -0300

    adicionado

---
+++ src/components/Citacao.vue
@@ -0,0 +1,121 @@
+<template>
+<section class="citacao">
+<div class="container">
+    <div class="row">
+<div class="col-md-12">
+<p class="citacao__text">
+“Lorem Ipsum is simply dummy text of the printing
+and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and
+scrambled it to make a type specimen book. “
+</p>
+<p class="citacao__author">(Mano Bronw)</p>
+</div>
+</div>
+</div>
+</section>
+</template>
+<script>
+export default {
+name : "Citacao"
+}
+</script>
+<style>
+html, body {
+margin: 0px;
+padding: 0px;
+font-family: sans-serif
+}
+.container {
+width: 100%;
+max-width: 960px;
+height: 100%;
+display: flex;
+margin: auto;
+flex-wrap: wrap;
+}
+.row {
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+align-items: center;
+flex: 1;
+width: 100%;
+}
+.col-md-1{
+width: calc(960px/12 * 1);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-2{
+width: calc(960px/12 * 2);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-3{
+width: calc(960px/12 * 3);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-4{
+width: calc(960px/12 * 4);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-5{
+width: calc(960px/12 * 5);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-6{
+width: calc(960px/12 * 6);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-7{
+width: calc(960px/12 * 7);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-8{
+width: calc(960px/12 * 8);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-9{
+width: calc(960px/12 * 9);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-10{
+width: calc(960px/12 * 10);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-11{
+width: calc(960px/12 * 11);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.col-md-12{
+width: calc(960px/12 * 12);
+box-sizing: border-box;
+padding: 0px 10px
+}
+.citacao {
+background-color : #0A0A0A;
+width: 100%;
+height: 255px;
+}
+.citacao .citacao__text {
+color: #C3121C;
+font-size : 24px;
+font-style: italic;
+}
+.citacao .citacao__author {
+color: #C3121C;
+font-size : 16px;
+text-align: right;
+}
+</style>
---
+++ src/components/Discografia.vue
@@ -0,0 +1,64 @@
+<template>
+<section class="discografia">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h2>Discografia</h2>
+</div>
+</div>
+<div class="row">
+aqui vai album
+</div>
+</div>
+</section>
+</template>
+<script>
+export default {
+name: "Discografia",
+}
+</script>
+<style>
+.discografia {
+padding : 56px 0;
+background-color: #212121;
+color: #e2e2e2;
+}
+.discografia h2 {
+padding-bottom : 30px;
+}
+.discografia__card{
+display: flex;
+flex-direction : column;
+justify-content: center;
+align-items: center;
+height: 340px;
+}
+.discografia__card img{
+width: 100%;
+max-width: 193px;
+height: 193px;
+}
+.discografia__card a{
+display: block;
+width: 70px;
+height : 30px;
+background-color: #e2e2e2;
+color: #212121;
+text-align: center;
+text-decoration : none;
+line-height: 31px;
+margin-top: 10px;
+margin-bottom: 30px;
+border-radius: 6px;
+font-size: 14px;
+font-weight: 900;
+}
+.discografia__card h5{
+font-size: 16px;
+text-align: center;
+padding: 0px;
+display: inline-block;
+max-width : 120px;
+margin: auto
+}
+</style>
